bb3fe1209e3ed608baaf4856c4a1e583656d53b6
Peter Teuben <teuben@gmail.com>
2026-02-27 12:57:59 -0500

    renamed

--- scripts/dysh_plot1.py
+++ scripts/dysh_plot1.py
@@ -1,18 +1,21 @@
 #! /usr/bin/env python
 #
-#    one simple dysh plot in batch mode
+#    one simple dysh plot, switching between interactive and batch mode
 
 import sys
 Qbatch = '--batch' in sys.argv       # cheap CLI parsing solution
 print("batch:",Qbatch)
 
+# make a plot
 from dysh.spectra.spectrum import Spectrum
 f = Spectrum.fake_spectrum()
 p = f.plot()
 
+# save the plot regardless of mode
 png = 'fake_plot1.png'
 p.savefig(png)
 print(f"Wrote {png}")
 
+# running as script, blocking is needed to show the figure on screen
 if not Qbatch:
     p.show(block=True)   # oddly, False is the default
