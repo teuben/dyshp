9e2390cb970fdeda67d16a9b0531fbdf5c4817da
Peter Teuben <teuben@gmail.com>
2026-03-03 13:34:22 -0500

    upd

--- scripts/mpl_simple1.py
+++ scripts/mpl_simple1.py
@@ -18,6 +18,10 @@ print("interactive:",plt.isinteractive())  # ion/ioff
 plt.figure()
 plt.plot([0,3,1])
 plt.title("single plot")
-plt.savefig("mpl_simple1.png")       # always save a figure?
+
+png = "mpl_simple1.png"
+plt.savefig(png)                     # always save a figure?
+print(f"Saved {png}")
+
 if not Qbatch:
     plt.show()                       # not needed in batch mode
